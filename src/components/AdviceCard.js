7d58e95de342c34712bd6661ded75eea240de6ec
KULBHUSHAN <krishnathakur0007895@gmail.com>
2024-01-18 21:54:14 +0530

    successfully configured api, fetched advises and rendered on screen

--- src/components/AdviceCard.js
+++ src/components/AdviceCard.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import dividerDesktop from '../images/pattern-divider-desktop.svg';
 import dividerMobile from '../images/pattern-divider-mobile.svg';
 import dice from '../images/icon-dice.svg'
+import axios from 'axios'
 const AdviceCard = () => {
+    const [advice, setadvice] = useState("");
+
+    const getAdvice = async () => {
+        const response = await axios.get("	https://api.adviceslip.com/advice");
+        // console.log(response);
+        const advice = await response.data.slip;
+        setadvice(advice);
+    }
+
+    useEffect(()=>{
+        getAdvice();
+    }, [])
     return (
         <div className="card">
-            <p>ADVICE #1</p>
-            <h2>This is an advice</h2>
+            <p>ADVICE #{advice.id}</p>
+            <h2>{advice.advice}</h2>
             <img src={dividerDesktop} className="divider-desktop" alt="divider" />
             <img src={dividerMobile} className="divider-mobile" alt="divider" />
-            <div className="dice">
+            <div className="dice" onClick={getAdvice}>
                 <img src={dice} alt="dice" />
             </div>
-                
+
         </div>
     )
 }
